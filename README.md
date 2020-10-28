5fb6137b6d6f957740c552b8d384214171a5daa8
marius <mariusndini@yahoo.com>
2020-10-28 08:51:23 -0400

    updated code
    updated for more then one code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# XML2JS
+# XML2JS 
 XML to JSON Convert
+
+Must have node.js installed beforehand
+
+
+Run <b>npm install</b> to install node.js dependencies
+
+
+run <b>node index.js</b> to convert files
+
+
+All XML files in the <b>input</b> folder will be converted and saved in the <b>output</b> folder.
+
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var parseString = require('xml2js').parseString;
 var fs = require('fs');
 
-var data = fs.readFileSync('rawdata.txt', 'utf8');
+//read all the files in the input directory
+fs.readdir('./input', readDir);
 
-parseString(data, function (err, result) {
-    console.log(result);
+//Do this action for the file
+var currentFileName = '';
+function readDir(err, files){
+    //for each file do this action
+    files.forEach(file => {
+        currentFileName = file;
+        parseString(fs.readFileSync('./input/'+file, 'utf8'), parseLogic); // convert the file
+    });
+}
 
-    fs.writeFile("./data.json", JSON.stringify(result), function(err) {
+//Prase THIS or one paricular file
+function parseLogic(err, result){
+    //write each file to the output folder (must exist beforehand)
+    fs.writeFile("./output/" + currentFileName + '.json', JSON.stringify(result), function(err) {
         if(err) {
             return console.log(err);
         }
-        console.log("The file was saved!");
+        console.log("FILE SAVED!");
     }); 
+}
 
-});
+
+
